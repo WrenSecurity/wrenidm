c0899621bec97792c79a5adf92f5e449233e59ad
Kortanul <kortanul@protonmail.com>
2018-02-25 20:02:33 -0500

    Add Wren Copyright to Changes from #19

--- openidm-api-servlet/src/main/java/org/forgerock/openidm/servlet/internal/ServletConnectionFactory.java
+++ openidm-api-servlet/src/main/java/org/forgerock/openidm/servlet/internal/ServletConnectionFactory.java
@@ -12,6 +12,7 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyright 2014-2016 ForgeRock AS.
+ * Copyright 2017-2018 Wren Security.
  */
 
 package org.forgerock.openidm.servlet.internal;
